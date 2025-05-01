b0bf65cbf870e78f57be76160a5f603b8db9cd73
José Cabrera <deltadax18@gmail.com>
2025-04-30 21:44:35 -0600

    feat (indexdb): integrate indexdb to save offline data from APIs

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,8 @@
-export const MOBILE_WIDTH = 768;
+export const SECTION_LEADERBOARD = "leaderboard"
+export const SECTION_MARKET = "market"
+
+//Index DB
+export const DB_NAME = "BloqueFishingDB";
+export const STORE_NAME = "leaderboardStore";
+export const LOCAL_LEADERBOARD_KEY = 'fishingData';
+export const LOCAL_MARKET_KEY = 'marketData';
