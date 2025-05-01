0a745be54b58397375ab29af20bba0e6b47afbeb
José Cabrera <deltadax18@gmail.com>
2025-04-30 21:59:01 -0600

    feat(responsive): fixes the responsive design, and adds tabs for smaller devices
    Tabs help using the space available in a better way and only showing whats necesary

--- src/app.js
+++ src/app.js
@@ -1,53 +1,28 @@
-import { useState, useEffect } from "preact/hooks";
+import { useState } from "preact/hooks";
 import "./app.css";
 import { html } from "./utils/html.js";
-import LeaderBoard from "./features/leaderboard/leaderboard.js";
-import { SECTION_LEADERBOARD, SECTION_MARKET } from "./features/leaderboard/constants.js";
-import { MOBILE_WIDTH } from "./constants.js";
+import LeaderBoard from "./features/leaderboard";
+import Market from "./features/market";
+import { SECTION_LEADERBOARD, SECTION_MARKET } from "./constants.js";
 
 const App = () => {
-  const [activeSection, setActiveSection] = useState(null);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= MOBILE_WIDTH);      
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const handleSectionClick = (section) => {
-    if (isMobile) {
-      setActiveSection(activeSection ? null : section);
-    }
-  };
+  const [activeSection, setActiveSection] = useState(SECTION_LEADERBOARD);
 
   return html`
-   <main className="flex flex-col space-y-2 min-h-[90vh] max-h-[90vh] min-w-[100vw] p-5" onClick=${(e) => {
-      if (isMobile && e.target === e.currentTarget) {
-        e.stopPropagation();
-        setActiveSection(null);
-      }
-    }}>
-      ${(!isMobile || activeSection === null || activeSection === SECTION_LEADERBOARD) &&
-        html`
-          <div>
-            <section className=${`flex basis-auto ${activeSection ? 'min-h-[90vh] max-h-[90vh]' : 'min-h-[45vh] max-h-[45vh]'} overflow-y-auto cursor-pointer`}>
-              <${LeaderBoard} isMobile=${isMobile} active=${activeSection === SECTION_LEADERBOARD} handleSelection=${handleSectionClick} />
-            </section>
-          </div>
-        `}
-      ${(!isMobile || activeSection === null || activeSection === SECTION_MARKET) &&
-        html`
-          <div onClick=${() => handleSectionClick("marketplace")}>
-            <section className=${`flex basis-auto ${activeSection ? 'min-h-[90vh] max-h-[90vh]' : 'min-h-[45vh] max-h-[45vh]'} overflow-y-auto cursor-pointer`}>
-              <h1 className="text-2xl font-bold text-green-600">marketplace</h1>
-            </section>
-          </div>
-        `}
-    </main>
+   <main className="flex flex-col lg:flex-row min-h-[100vh] max-h-[100vh] min-w-[100vw]">
+    <div className="flex flex-col lg:flex-row space-y-2 xl:space-x-5 lg:justify-between lg:p-5 lg:pt-10 xl:pt-5 h-full w-full ">
+      <section className=${`flex basis-auto lg:basis-full ${activeSection !== SECTION_LEADERBOARD ? 'hidden lg:hidden xl:flex' : ''} min-h-[95vh] lg:min-h-[92vh] xl:min-h-[95vh] max-h-[95vh] md:min-h-[90vh] md:max-h-[90vh] overflow-y-auto`}>
+        <${LeaderBoard} />
+      </section>
+      <section className=${`flex basis-auto lg:basis-full ${activeSection !== SECTION_MARKET ? 'hidden lg:hidden xl:flex' : ''} min-h-[95vh] lg:min-h-[92vh] xl:min-h-[95vh] max-h-[95vh] md:min-h-[90vh] md:max-h-[90vh] overflow-y-auto`}>
+        <${Market} />
+      </section>
+    </div>    
+    <div className="xl:hidden fixed bottom-0 left-0 right-0 lg:top-0 lg:left-0 lg:right-0 rounded-t-md lg:rounded-none lg:rounded-b-md  bg-white dark:bg-gray-800 border-t dark:border-gray-700 flex  justify-around h-8 z-100">
+      <button className=${`text-sm font-semibold ${activeSection === SECTION_LEADERBOARD ? 'text-green-700 dark:text-green-400' : 'text-gray-500 dark:text-gray-400'} border-r-2 border-r-green-700 dark:border-r-gray-700 w-full h-full`} onClick=${() => setActiveSection(SECTION_LEADERBOARD)}>Leaderboard</button>
+      <button className=${`text-sm font-semibold ${activeSection === SECTION_MARKET ? 'text-green-700 dark:text-green-400' : 'text-gray-500 dark:text-gray-400'} w-full h-full`} onClick=${() => setActiveSection(SECTION_MARKET)}>Marketplace</button>
+    </div>    
+  </main>
   `;
 };
 
